13377f788fdbb189baa512c50208b3c74a446ce1
Pavel Kuzmich <kuzmich@sovanet.cz>
2024-10-13 12:19:48 +0200

    Initial commit

---
+++ test/temp_sdk_stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TempSdkStack from '../lib/temp_sdk_stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/temp_sdk_stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TempSdkStack.TempSdkStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
