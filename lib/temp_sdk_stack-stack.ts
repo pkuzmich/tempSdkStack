778fd682eed605abadb399890089167b5b48e3bd
Pavel Kuzmich <kuzmich@sovanet.cz>
2024-10-26 22:50:41 +0200

    Add prettier, update package, add lambda

--- lib/temp_sdk_stack-stack.ts
+++ lib/temp_sdk_stack-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import type { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class TempSdkStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'TempSdkStackQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new lambdaNodejs.NodejsFunction(this, 'timeOfDay', {
+      entry: './lambda/timeOfDay.js',
+      handler: 'index',
+      runtime: lambda.Runtime.NODEJS_20_X,
+    });
   }
 }
